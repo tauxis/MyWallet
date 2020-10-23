d1e18c3deed7a6a5933d6667beabc6ada473553c
Boutillier-Cyril <cyril.boutillier.62@gmail.com>
2020-10-23 17:04:38 +0200

    [ADD] Prise d'une photo et affichage en local de celle ci

--- app/src/main/java/com/ccm2/projet/thematique/mywallet/menu/MenuActivity.kt
+++ app/src/main/java/com/ccm2/projet/thematique/mywallet/menu/MenuActivity.kt
@@ -1,12 +1,22 @@
 package com.ccm2.projet.thematique.mywallet.menu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ccm2.projet.thematique.mywallet.R
+import com.ccm2.projet.thematique.mywallet.photoactivity.PhotoActivity
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        addPhoto.setOnClickListener() {
+            goToAppareilPhoto();
+        }
+    }
+
+    private fun goToAppareilPhoto() {
+        startActivity(Intent(this, PhotoActivity::class.java));
     }
 }
