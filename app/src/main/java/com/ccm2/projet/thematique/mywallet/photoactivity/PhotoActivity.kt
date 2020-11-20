12671c0db05f58193b103101aeb1831989bdd6a2
Boutillier-Cyril <cyril.boutillier.62@gmail.com>
2020-11-20 16:57:55 +0100

    [UPDATE] Crop image

--- app/src/main/java/com/ccm2/projet/thematique/mywallet/photoactivity/PhotoActivity.kt
+++ app/src/main/java/com/ccm2/projet/thematique/mywallet/photoactivity/PhotoActivity.kt
@@ -1,12 +1,9 @@
 package com.ccm2.projet.thematique.mywallet.photoactivity
 
-import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.ccm2.projet.thematique.mywallet.R
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
@@ -18,23 +15,30 @@ class PhotoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo)
-        button.setOnClickListener {
+        CropImage.activity()
+            .setGuidelines(CropImageView.Guidelines.ON)
+            .start(this);
+        restart_photo.setOnClickListener {
             CropImage.activity()
                 .setGuidelines(CropImageView.Guidelines.ON)
                 .start(this);
         }
+        valid_photo.setOnClickListener {
+            // TODO : Envoyer le bitmap vers Drive
+        }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-            CAMERA_REQUEST_CODE -> {
-                if (resultCode == Activity.RESULT_OK && data != null) {
-                    imageView.setImageBitmap(data.extras?.get("data") as Bitmap)
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(data)
+            if (resultCode == RESULT_OK) {
+                val resultUri: Uri = result.uri
 
-                }
-            }
-            else -> {
-                Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT)
+                cropImageView.setImageURI(resultUri);
+//                cropImageView.getCroppedImageAsync();
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                val error = result.error
+                System.out.println(error);
             }
         }
     }
