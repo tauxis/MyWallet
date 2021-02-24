97604642c17fa1f77a9a1f08c6e833fcd284ddbe
Boutillier-Cyril <cyril.boutillier.62@gmail.com>
2021-02-24 11:17:26 +0100

    [jesaisplus]

--- app/src/main/java/com/ccm2/projet/thematique/mywallet/storage/ItemAdapter.kt
+++ app/src/main/java/com/ccm2/projet/thematique/mywallet/storage/ItemAdapter.kt
@@ -16,6 +16,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.ccm2.projet.thematique.mywallet.R
+import com.ccm2.projet.thematique.mywallet.fileio.FileIO
 import com.ccm2.projet.thematique.mywallet.mailactivity.MailActivity
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
@@ -24,6 +25,8 @@ import com.google.firebase.storage.FileDownloadTask
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Files.exists
 
 
 class ItemAdapter(private val context: Context, var items: ArrayList<StorageItem>):
@@ -62,10 +65,11 @@ class ItemAdapter(private val context: Context, var items: ArrayList<StorageItem
         builder.setNegativeButton("Envoyer") { dialog, which ->
             //@TODO créer le lien avec l'activité Mail
             val intent = Intent(context, MailActivity::class.java)
-            downloadItem(items.get(position))
+
+            downloadItem(item.itemPath)
 //            intent.putExtra("items", items)
 //            intent.putExtra("position", position)
-//            context.startActivity(intent);
+            context.startActivity(intent);
         }
         val dialog = builder.create()
         dialog.show()
@@ -77,40 +81,37 @@ class ItemAdapter(private val context: Context, var items: ArrayList<StorageItem
         tmpStorageItem.delete()
     }
 
-    private fun downloadItem(item: StorageItem){
-        val tmpStorageItem = firebaseStorage.reference.child(item.itemPath)
+    private fun downloadItem(itemPath: String){
+        val tmpStorageItem = firebaseStorage.reference.child(itemPath)
         val pd = ProgressDialog(context)
-        pd.setTitle(item.itemName)
+        pd.setTitle(tmpStorageItem.name)
         pd.setMessage("Downloading Please Wait!")
         pd.isIndeterminate = true
         pd.setProgressStyle(ProgressDialog.STYLE_SPINNER)
         pd.show()
 
-        val rootPath = File(Environment.getExternalStorageDirectory(), "MADBO DOWNLOADS")
-
-        if (!rootPath.exists()) {
+        val rootPath = File(context.filesDir,"download")
+        System.out.println(rootPath)
+        System.out.println(!rootPath.exists())
+        if (rootPath.absoluteFile.exists()) {
             rootPath.mkdirs()
+            System.out.println("Wesh les gros lards")
+
         }
 
-        val localFile = File(rootPath, item.itemName)
+        val localFile = File(rootPath, tmpStorageItem.name + ".jpg")
 
         tmpStorageItem.getFile(localFile)
-            .addOnSuccessListener(object : OnSuccessListener<FileDownloadTask.TaskSnapshot?> {
-                override fun onSuccess(taskSnapshot: FileDownloadTask.TaskSnapshot?) {
-                    Log.e("firebase ", ";local tem file created  created $localFile")
-                    if (localFile.canRead()) {
-                        pd.dismiss()
-                    }
-                    Toast.makeText(context, "Download Completed", Toast.LENGTH_SHORT).show()
+            .addOnSuccessListener {
+                Log.e("firebase ", ";local tem file created  created $localFile")
+                if (localFile.canRead()) {
+                    pd.dismiss()
                 }
-            }).addOnFailureListener(object : OnFailureListener {
-                override fun onFailure(exception: Exception) {
-                    System.out.println(item.itemUrl)
-                    System.out.println(item.itemPath)
-                    Log.e("firebase ", ";local tem file not created  created $exception")
-                    Toast.makeText(context, "Download Incompleted", Toast.LENGTH_LONG).show()
-                }
-            })
+                Toast.makeText(context, "Download Completed", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener { exception ->
+                Log.e("firebase ", ";local tem file not created  created $exception")
+                Toast.makeText(context, "Download Incompleted", Toast.LENGTH_LONG).show()
+            }
     }
 
     class ViewHolder(view: View):RecyclerView.ViewHolder(view) {
